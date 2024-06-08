5470942f6644c2eca7acf8c320be2dfbbfd54b30
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-06-09 01:14:36 +0545

    finalizing

--- .history/propose/script_20240609010941.js
+++
@@ -1,7 +0,0 @@
-const yesButton = document.getElementById("yes");
-const noButton = document.getElementById("no");
-const cta = document.querySelector(".buttons");
-
-noButton.addEventListener("mouseenter", () => {
-  const yesIndex = Array.from(cta.children).indexOf(noButton);
-});
