c95d9e1548fcc0dd74795e68231f809b787e77e9
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-06-15 13:24:51 +0545

    update

--- propose/script.js
+++ propose/script.js
@@ -2,9 +2,9 @@ const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
 const swp = document.querySelector(".buttons");
 
- button.addEventListener("click", function() {
-        console.log("Button clicked!");
-    });
+yesButton.addEventListener("click", function () {
+  console.log("Button clicked!");
+});
 
 noButton.addEventListener("mouseenter", () => {
   const yesIndex = Array.from(swp.children).indexOf(yesButton);
